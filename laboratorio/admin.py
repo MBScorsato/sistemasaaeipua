0622f76558f6ca29302a94b93fd16ee4efc3d0f1
Murilo B. Scorsato <scorsatomurilo@gmail.com>
2024-02-13 10:19:07 -0300

    13/02/2024

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from laboratorio.models import Numero_Media, Controle_Operacional, Cadastro_Reservatorio, Reservatorio, \
-    Anotacoes, Organiza_tarefa
+    Anotacoes, Organiza_tarefa, Informacoes_Analises_Basicas_Interna, Banco_Reservatorio_temporal
 
 admin.site.register(Numero_Media)
 admin.site.register(Controle_Operacional)
@@ -9,3 +9,5 @@ admin.site.register(Cadastro_Reservatorio)
 admin.site.register(Reservatorio)
 admin.site.register(Anotacoes)
 admin.site.register(Organiza_tarefa)
+admin.site.register(Informacoes_Analises_Basicas_Interna)
+admin.site.register(Banco_Reservatorio_temporal)
